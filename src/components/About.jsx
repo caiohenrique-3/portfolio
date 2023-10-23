8e3567fb4292227da077e7de5d29181486fc0417
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2023-10-23 17:12:37 -0300

    fix: Resolve cv download broken link

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,14 +6,14 @@ export default function About({ language }) {
       heading: "About me",
       description1:
         "I'm Caio Henrique, a frontend developer who finds beauty in simplicity. Much like my preference for Linux, I value systems that offer users freedom and follow a minimalist philosophy. When it comes to my websites, I prioritize accessibility and steer clear of unnecessary elements. My aim is to contribute to a cleaner, more user-friendly internet experience.",
-      cvLink: "portfolio/test.pdf",
+      cvLink: "./test.pdf",
       cvDownloadLabel: "Download CV",
     },
     "pt-BR": {
       heading: "Sobre mim",
       description1:
         "Sou Caio Henrique, um desenvolvedor front-end que encontra beleza na simplicidade. Assim como minha preferência pelo Linux, valorizo sistemas que oferecem liberdade aos usuários e seguem uma filosofia minimalista. Quando se trata de meus sites, priorizo a acessibilidade e evito elementos desnecessários. Meu objetivo é contribuir para uma experiênciade Internet mais limpa e fácil de usar.",
-      cvLink: "portfolio/testPtBR.pdf",
+      cvLink: "./testPtBR.pdf",
       cvDownloadLabel: "Download currículo",
     },
   };
