463342cb656d5d321b2cb0bcda9341f2885f36f5
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2023-10-23 18:48:18 -0300

    feat: Display country code only in en-US version

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,18 +5,20 @@ export default function Contact({ language }) {
     en: {
       heading: "Contact Me",
       phoneLabel: "Phone",
+      phoneNumber: "+51 (21) 99350-2805",
       linkedinLabel: "LinkedIn Profile",
       githubLabel: "GitHub Profile",
     },
     "pt-BR": {
       heading: "Contato",
       phoneLabel: "Telefone",
+      phoneNumber: "(21) 99350-2805",
       linkedinLabel: "Perfil no LinkedIn",
       githubLabel: "Perfil no GitHub",
     },
   };
 
-  const { heading, phoneLabel, linkedinLabel, githubLabel } =
+  const { heading, phoneLabel, phoneNumber, linkedinLabel, githubLabel } =
     translations[language];
   return (
     <>
@@ -24,7 +26,7 @@ export default function Contact({ language }) {
         <h2>{heading}</h2>
         <ul>
           <li>Email: caiohenrique29@protonmail.com</li>
-          <li>{phoneLabel}: +51 21 99350-2805</li>
+          <li>{phoneLabel}: {phoneNumber}</li>
           <li>
             <a href="https://www.linkedin.com/in/caio-henrique-c7351/">
               <i className="fa fa-external-link" aria-hidden="true"></i>
