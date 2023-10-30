dc39e69833471c1dc25516a0d38fd88162fd22f0
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2023-10-30 18:20:35 -0300

    chore: Add new project

--- src/data/projectsData.jsx
+++ src/data/projectsData.jsx
@@ -229,4 +229,44 @@ export default [
       "pt-BR": "Código fonte",
     },
   },
+
+  {
+    id: 7,
+    title: {
+      en: "Tenzies Game",
+      "pt-BR": "Jogo Tenzies",
+    },
+    description: {
+      en: "A browser tenzies game.",
+      "pt-BR": "Um jogo Tenzies de navegador.",
+    },
+    img:
+      "https://github.com/caiohenrique-3/tenzies/blob/main/src/assets/showcase.png?raw=true",
+    imgAlt: {
+      en: "Image of the project",
+      "pt-BR": "Imagem do projeto",
+    },
+    showImageLabel: {
+      en: "Click to show image",
+      "pt-BR": "Clique para exibir a imagem",
+    },
+    hideImageLabel: {
+      en: "Click to hide image",
+      "pt-BR": "Clique para ocultar a imagem",
+    },
+    technologies: {
+      en: "Technologies used: React, Git",
+      "pt-BR": "Tecnologias usadas: React, Git",
+    },
+    demo: "https://caiohenrique-3.github.io/tenzies/",
+    demoLabel: {
+      en: "Demo",
+      "pt-BR": "Demonstração",
+    },
+    sourceCode: "https://github.com/caiohenrique-3/tenzies",
+    sourceCodeLabel: {
+      en: "Source code",
+      "pt-BR": "Código fonte",
+    },
+  },
 ];
