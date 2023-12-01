c5dd9f2b016834dd810f3df994cbfc74f27b6951
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2023-12-01 17:43:45 -0300

    feat: Add header

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,48 @@
 <script>
-  import Test from "./lib/Test.svelte";
+  import Header from "./lib/Header.svelte";
 </script>
 
-<main class="flex justify-center text-center items-center h-screen">
-  <h1 class="text-3xl rounded-lg p-6 bg-green-800 text-white">Hello, this is a <Test /></h1>
-</main>
-
+<div class="min-h-screen w-full bg-slate-800">
+  <Header />
+  <div class="p-6 text-white">
+    Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim
+    labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet.
+    Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum
+    Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident.
+    Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex
+    occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat
+    officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in
+    Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non
+    excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut
+    ea consectetur et est culpa et culpa duis. Lorem ipsum dolor sit amet,
+    officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi
+    Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat
+    excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip
+    amet voluptate voluptate dolor minim nulla est proident. Nostrud officia
+    pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat
+    reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia
+    voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt
+    duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis
+    sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur
+    et est culpa et culpa duis. Lorem ipsum dolor sit amet, officia excepteur ex
+    fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex
+    esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit
+    nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim
+    nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse
+    ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis
+    laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla
+    nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit
+    commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint
+    cupidatat ullamco ut ea consectetur et est culpa et culpa duis. Lorem ipsum
+    dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa
+    sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim
+    cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est
+    aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud
+    officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat
+    reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia
+    voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt
+    duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis
+    sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur
+    et est culpa et culpa duis.
+  </div>
+</div>
---
+++ src/lib/Header.svelte
@@ -0,0 +1,25 @@
+<header
+  class="flex p-2 justify-center items-center text-center sticky inset-x-0 top-0"
+>
+  <nav class="flex">
+    <ul class="flex list-none text-white">
+      <li class="px-2 sm:px-4">
+        <a href="#sobre" class="hover:text-emerald-300 sm:text-2xl">Sobre</a>
+      </li>
+      <li class="px-2 sm:px-4">
+        <a href="#projetos" class="hover:text-emerald-300 sm:text-2xl"
+          >Projetos</a
+        >
+      </li>
+      <li class="px-2 sm:px-4">
+        <a href="#habilidades" class="hover:text-emerald-300 sm:text-2xl"
+          >Habilidades</a
+        >
+      </li>
+      <li class="px-2 sm:px-4">
+        <a href="#contato" class="hover:text-emerald-300 sm:text-2xl">Contato</a
+        >
+      </li>
+    </ul>
+  </nav>
+</header>
