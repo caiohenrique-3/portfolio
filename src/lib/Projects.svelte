ece6ccaa8923f75cbde3e9e3a21bd6638f1206dd
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2023-12-04 11:52:29 -0300

    feat: Add sort functionality to Projects section

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -8,6 +8,7 @@
   // data
   import projects from "../data/projects";
 
+  // Show/hide project showcase
   let showImage = {};
 
   onMount(() => {
@@ -19,12 +20,40 @@
   function handleClick(id) {
     showImage[id] = !showImage[id];
   }
+
+  // Display projects based on category
+  let currentCategory = "All";
+
+  // Counting number of projects in category
+  function countProjects(category) {
+    if (category === "All") {
+      return projects.length;
+    } else {
+      return projects.filter((project) =>
+        project.technologies.includes(category)
+      ).length;
+    }
+  }
 </script>
 
 <main id="projetos" class="text-white p-6 max-w-4xl mx-auto">
   <h2 class="text-3xl text-emerald-300 pb-3">Projetos</h2>
+
+  <button on:click={() => (currentCategory = "All")}
+    >All ({countProjects("All")})</button
+  >
+  <button on:click={() => (currentCategory = "JavaScript")}
+    >JavaScript ({countProjects("JavaScript")})</button
+  >
+  <button on:click={() => (currentCategory = "React")}
+    >React ({countProjects("React")})</button
+  >
+  <button on:click={() => (currentCategory = "Java")}
+    >Java ({countProjects("Java")})</button
+  >
+
   <swiper-container loop="true" slides-per-view="2">
-    {#each projects as project (project.id)}
+    {#each projects.filter((project) => currentCategory === "All" || project.technologies.includes(currentCategory)) as project (project.id)}
       <swiper-slide key={project.id}>
         <div class="project">
           <h3>{project.title}</h3>
